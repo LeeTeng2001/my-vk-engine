014ef3e6918d90d298aa7ce73efc504669f6d05b
Bryan Lee <leeteng2001@gmail.com>
2023-12-12 15:04:42 +0800

    add tinygltf dependencies

--- src/engine/lib_impl.cpp
+++ src/engine/lib_impl.cpp
@@ -3,3 +3,10 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE
+#define TINYGLTF_NO_INCLUDE_STB_IMAGE_WRITE
+#define TINYGLTF_IMPLEMENTATION
+#include <tiny_gltf.h>
